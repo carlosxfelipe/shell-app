53ff55abe1b45aaaba495250d578f0b67f63abe8
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-02-08 21:56:36 -0300

    Add SplashScreen component and integrate it into HomeScreen

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -5,10 +5,13 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParams} from '../../routes/StackNavigator';
 import {GitHubBadge} from '../../components/GitHubBadge';
+import {useState} from 'react';
+import {SplashScreen} from '../splash/SplashScreen';
 
 export const HomeScreen: React.FC = () => {
   const globalStyles = useGlobalStyles();
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>();
+  const [showSplash, setShowSplash] = useState(false);
 
   return (
     <ScrollView
@@ -18,6 +21,13 @@ export const HomeScreen: React.FC = () => {
         label="Testar Carousel"
         onPress={() => navigation.navigate('NewCarouselScreen')}
       />
+
+      <PrimaryButton
+        label="Testar SplashScreen"
+        onPress={() => setShowSplash(true)}
+      />
+      <SplashScreen visible={showSplash} onClose={() => setShowSplash(false)} />
+
       <GitHubBadge
         username="@carlosxfelipe"
         projectUrl="https://github.com/carlosxfelipe/shell-app"
